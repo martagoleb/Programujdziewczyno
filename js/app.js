f1512bb3986b94febc2ff7351ce7b6c1f446c53c
Marta Goleb <magika77@wp.pl>
2017-10-21 15:59:37 +0200

    js dodany

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("działa!");
+var myName = 'Marta';
+var names = ['Agata', 'Marta', 'Basia', 'Asia'];
+console.log(myName);
+
+var a = 150;
+var b = 3;
+
+if (a > b) {console.log('hurra!')
+} else {console.log('ojej')
+}
+
+for (var i = 0; i <= names.length-1; i++) {
+console.log(names[i]);}
